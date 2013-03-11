9d2b55ad300b1522a8ae448f470aaa5c8f554b2a
Allen Gay <allenarthurgay@gmail.com>
2013-03-10 23:04:40 -0700

    initial commit

---
+++ login.js
@@ -0,0 +1,38 @@
+define(['angular'], function (angular) {
+    /* Directives */
+    var templateUrl = require.toUrl('./login.html');
+    angular.module('angular-login.directives', ['angular-login.authentication'])
+        .directive('appLogin', ['version', 'auth', function (version, auth) {
+            return {
+                restrict: 'E',
+                templateUrl: templateUrl,
+                scope: {},
+                controller: function ($scope) {
+                    $scope.login = function () {
+                        console.log($scope.credentials);
+                        auth.login($scope.credentials.email, $scope.credentials.password);
+                    }
+                }
+            };
+        }]);
+
+
+    angular.module('angular-login.authentication', []).
+        factory('auth', function ($http) {
+            function login(username, password) {
+                alert(username + "," + password);
+                $http.get('/login')
+                    .success(function (data, status, header, config) {
+
+                    })
+                    .error(function (data, status, headers, config) {
+
+                    });
+            }
+
+            return {
+                login: login
+            };
+        });
+});
+
