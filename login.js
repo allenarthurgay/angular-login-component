9020de8f14f1c14b2fe10d6f257ce8cf1ba35ea3
Allen Gay <allenarthurgay@gmail.com>
2013-04-18 08:50:13 -0700

    oops, copied twice

--- login.js
+++ login.js
@@ -43,48 +43,4 @@ define(['angular'], function (angular) {
     });
 });
 
-define(['angular'], function (angular) {
-
-    var templateUrl = require.toUrl('./login.html'),
-        module = angular.module('angular-login', []),
-        directives = {},
-        services = {};
-
-    module.directive('appLogin', ['auth', function (auth) {
-        return {
-            restrict: 'E',
-            templateUrl: templateUrl,
-            scope: {},
-            controller: function ($scope) {
-                $scope.login = function () {
-                    console.log($scope.credentials);
-                    auth.login($scope.credentials.email, $scope.credentials.password);
-                }
-            }
-        };
-    }]);
-
-    module.factory('auth', function ($http) {
-        function login(username, password) {
-            console.log(username + "," + password);
-            return true;
-            /*
-             $http.get('/login')
-             .success(function (data, status, header, config) {
-
-             })
-             .error(function (data, status, headers, config) {
-
-             }); */
-        }
-
-        function isLoggedIn() {
-            return true;
-        }
-
-        return {
-            login: login
-        };
-    });
-});
 
